79e266df6101b64f0502d20c8e4a87f9032f9ccb
choijihye <choijihye295@jbnu.ac.kr>
2024-12-10 00:50:38 +0900

    congig db 개인정보 .env로 옮기기

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,12 @@
-// src/config/dbConfig.js
 const mysql = require('mysql2/promise');
+require('dotenv').config();  // .env 파일을 로드
 
 const pool = mysql.createPool({
-  host: '113.198.66.75',  
-  user: 'cjh',
-  password: 'fabia8990!',
-  database: 'saramin_db',
-  port: 10089,
+  host: process.env.DB_HOST,  // .env에서 읽어온 값
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   ssl: false,
